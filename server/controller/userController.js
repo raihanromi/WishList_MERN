9ff6a5d362642cd35bcff69e6878799e8f6ffca3
Raihan <raihanromi98@gmail.com>
2024-01-26 00:20:55 +0600

    hashed password feature added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,33 +1,41 @@
-//Have to add user functionality to the server
+//TODO: add user functionality to the server
 const User = require("../models/userModel");
-
+const bcrypt = require("bcryptjs");
 
 //save new user in the DB
 const newUser = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
   try {
-    const userinformation = await User.create(req.body);
+    const userinformation = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword,
+    });
+    await userinformation.save();
     res.send(userinformation);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-//verify user login 
-
-const checkUser = async ( req,res ) =>{
-  const email = req.body.email
-  const password = req.body.password
-  const verifyuser = await User.findOne({email})
-  if(verifyuser){
-  res.send(verifyuser)
-  
-  }else{
-    res.send("not a user ")
+//verify user login
+const checkUser = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const verifyuser = await User.findOne({ email });
+  if (verifyuser) {
+    const validPassword = await bcrypt.compare(password, verifyuser.password);
+    if (validPassword) {
+      console.log(verifyuser);
+    } else {
+      console.log("wrong password")
+      res.send("wrong password");
+    }
+  } else {
+    res.send("not a user ");
   }
-
-}
-
-
+};
 
 //get all user from the DB
 const allUser = async (req, res) => {
@@ -35,4 +43,4 @@ const allUser = async (req, res) => {
   res.send(alluser);
 };
 
-module.exports = { newUser, allUser ,checkUser};
+module.exports = { newUser, allUser, checkUser };
