6751e5ac496f1b02df9a6e6c39f2df404dcc96bc
Raihan <raihanromi98@gmail.com>
2024-03-13 00:38:53 +0600

    login bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes,Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
@@ -8,13 +8,13 @@ import "react-toastify/dist/ReactToastify.css";
 import LoginPage from "./pages/LoginPage";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
-import { useNavigate } from "react-router-dom";
+
 
 export const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
   const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
+
 
   return (
     <div>
@@ -38,13 +38,13 @@ const App = () => {
       </nav> */}
       <div className="container mx-auto p-2 h-full">
         <Routes>
-          <Route index element={user? <HomePage />: navigate("/login")}></Route>
+          <Route index element={ user? <HomePage />: <Navigate to="/login"/> }></Route>
           <Route path="/create" element={<CreatePage />}></Route>
           <Route path="/edit/:id" element={<EditPage />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
           <Route
             path="/login"
-            element={user?navigate("/"): <LoginPage />}
+            element={user?<Navigate to="/"/>: <LoginPage />}
           ></Route>
         </Routes>
       </div>
